74a63d9595bdbc16f055a24e90c9daa24bbff646
khadev <lekha3604@gmail.com>
2024-12-19 22:17:31 +0700

    first contribution

---
+++ calculation.js
@@ -0,0 +1,23 @@
+
+// tạo hàm
+function sum(x, y) {
+  return x + y
+}
+
+// function multiply(x, y){
+//   return x * y
+// }
+
+// tạo 1 biến const và gán nó vào 1 cái hàm
+const multiply = (x, y) => {
+  return x * y
+}
+
+// one - line function
+const substract = (x, y) => x - y 
+
+export {
+  sum,
+  multiply,
+  substract
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import usersRouter from './routers/users.js'
+dotenv.config()  // must have, gọi hàm config trong thư viện dotenv
+const app = express()
+const port = process.env.PORT || 3000
+// router
+app.use('/users', usersRouter)
+
+
+// thêm 1 cái root router khi client call đến đường dẫn root , tức là localhost:3002/
+app.get('/', (req, res) => {
+  res.send('respone from root router, haha')
+})
+app.listen(port, async() =>{
+  console.log(`listening on port: ${port}`)
+})
+
+/*
+import { sum, multiply, substract } from "./calculation.js"
+console.log('Hello world')
+console.log(`3 + 2 = ${sum(3, 2)}`)         
+console.log(`3 * 2 = ${multiply(3, 2)}`)                     
+console.log(`3 - 2 = ${substract(3, 2)}`)
+*/
