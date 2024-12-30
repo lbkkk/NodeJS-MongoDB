4c0eb01ce6d56d6de82c44dc4afe986f03482453
khadev <lekha3604@gmail.com>
2024-12-30 16:28:18 +0700

    update_2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/users', usersRouter)
 
 // thêm 1 cái root router khi client call đến đường dẫn root , tức là localhost:3002/
 app.get('/', (req, res) => {
-  res.send('respone from root router, haha123')
+  res.send('respone from root router, haha12345')
 })
 app.listen(port, async() =>{
   console.log(`listening on port: ${port}`)
